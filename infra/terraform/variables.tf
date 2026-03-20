770973c5d8facbafa178544ac217eac7a93e933e
kusupon <kusupon0628@gmail.com>
2026-03-20 14:42:19 +0900

    infra: Terraformのデフォルトリージョンをus-east-1に変更
    AWS の既定リージョンを ap-northeast-1 から us-east-1 に変更した。
    
    Terraform の変数定義のみを更新し、他の backend 変更とは分離して管理する。

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for the main stack."
   type        = string
-  default     = "ap-northeast-1"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
